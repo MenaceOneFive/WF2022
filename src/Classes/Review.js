aca7ec7931ad4ae16ccbaea55048bbb3247d54bd
HoyeonLee <dlghdus122@gmail.com>
2022-11-02 12:54:58 +0900

    리뷰를 추가하는 작업 POC

---
+++ src/Classes/Review.js
@@ -0,0 +1,22 @@
+export class Review{
+    constructor() {
+        this.UID = ""       //FirebaseUID
+        this.stars = 5      //유저 평가한 별점
+        this.review= ""     //리뷰
+    }
+}
+export const ReviewConverter = {
+    toFirestore: (review) => {
+        return {
+            uid: review.uid,
+            stars: review.stars,
+            review: review.review
+        }
+    }, fromFirestore: (snapshot, options) => {
+        const data = snapshot.data(options)
+        let review = new Review();
+        review.UID = data.UID
+        review.stars = data.stars
+        review.review = data.review;
+    }
+}
