bd98f7bff8bee43f713aa6cb6a0b18d8ccf2ee5f
MenaceOneFive <l2395@naver.com>
2022-11-05 02:54:54 +0900

    파이어스토어에서 데이터 가져오는 방법 수정

--- src/Modules/UI/MainPage/MainPageItem.js
+++ src/Modules/UI/MainPage/MainPageItem.js
@@ -1,23 +1,21 @@
-import {UploadReview} from "../../Firebase/UsefulButtons";
-import {ReviewSection} from "../../Review/UploadReview";
-
-export const MainPageItem = ({room, userState}) => {
+export const MainPageItem = ({room}) => {
+    console.log(room.name);
     const renderFacilities = (facility) => {
         if (facility == null)
             return (<></>)
-        return facility.map((object, idx) =>
-            (
-                    <ul key={idx} >
+        return facility.map((object, idx) => {
+            return (<>
+                    <ul key={idx}>
                         <li><h4>{object.head}</h4>
                             <ul>
                                 {object.items.map((item, i) => {
                                     return <li key={i}><h5>{item}</h5></li>
-                                })}
-                            </ul>
+                                })}</ul>
                         </li>
                     </ul>
+                </>
             )
-        )
+        })
     }
     const renderImage = (images) => {
         if (images == null)
@@ -25,39 +23,67 @@ export const MainPageItem = ({room, userState}) => {
         return (
             images.map(
                 (item, idx) => {
+                    console.log(item);
                     return <img src={item} key={idx} width={200} height={100}/>
                 }
             ))
     }
+    //
+    const renderOneImg = (images) => {
+        console.log(images[0]);
+        if(images == null)
+            return (<></>)
+
+        return (
+            <img src={images[0]} width={250} height={200}/>
+        )
+
+    }
+    //
+
+    const onClickRoomItem = (rooms, idx) => {
+        return(
+            <>
+            </>
+        )
+    }
+
+    //
     if (room == null)
         return (<></>)
     return (
         <>
-            <div><h1>{room.id}</h1></div>
-            <div>
+            <div className='room-container' onClick={onClickRoomItem}>
+                <div className='room-info'>
+                    {renderOneImg(room.images)}
+                    <h4 className='room-name'>{room.name}</h4>
+
+                </div>
             </div>
-            <div className={"images"}>{
-                renderImage(room.images)
-            }</div>
-            <div><h2>{room.name}</h2></div>
-            <div>
-                <ul><h3>설명 : {room.description}</h3></ul>
-            </div>
-            <div className={"facility"}>
-                <h2>시설</h2>
-                {renderFacilities(room.facility)
-                }</div>
-            <ReviewSection room={room} userState={userState}/>
         </>
     )
+    // <div>{renderImage(room.images)}</div>
 }
-
-export const ItemGroup = ({objs, idx, userState}) => {
+//
+export const ShowList = ({objs}) => {
+    if (objs.length == 0)
+        return (<></>)
+    console.log(objs);
+    return (<>
+        {objs.map(
+            (item, i)=>{
+                return <MainPageItem room={item} key={i}/>
+            }
+        )}
+    </>)
+}
+//
+export const ItemGroup = ({objs, idx}) => {
     if (objs.length == 0)
         return (<></>)
     return (<>
         {
-            <MainPageItem room={objs[idx]} userState={userState} />
+            <MainPageItem room={objs[idx]}/>
         }
     </>)
 }
