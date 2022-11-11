b99af92e7baec873f97610c7292452ee95eaf05b
chlwlsguszz <chlwlsguszz@gmail.com>
2022-11-11 19:30:14 +0900

    이메일 로그인 구현 / 로그인 UI
    이메일 + 패스워드 로그인 추가
    리액트 부트스트랩으로 UI 구성
    
    매핑 Error가 나서
    CarouselBanner 에서 bootstrap의 import 경로구성을 바꾸고 경로내 bootstrap.css를 삭제 ->  현재 환경에서 기능 이상 없음 확인 필요
    
    -----------
    '로그인 상태 유지' 체크박스 기능 미구현
    회원가입 페이지 추가 필요

--- src/Modules/UI/Common/SignInPage.js
+++ src/Modules/UI/Common/SignInPage.js
@@ -1,16 +1,75 @@
+import "./SignInPage.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Container, Form } from "react-bootstrap";
+
 import {useNavigate} from "react-router-dom";
 import {getFBAuth, isSignedIn, signOut} from "../../Firebase/FBAuth";
 import {GoogleSignInButton} from "../../Firebase/GoogleSignInButton";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
 
 export const SignInPage = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
     const auth = getFBAuth()
-    console.log(auth.currentUser==null? "":auth.displayName)
+    const navigate = useNavigate();
+
+    const onChange = (event) => {
+        const {target:{id, value}} = event;
+        if (id === "sign-in-email-address") {
+            setEmail(value);
+        }
+        else if (id === "sign-in-password") {
+            setPassword(value);
+        }
+    }   
+
+    const onSubmit = async(event) => {
+        event.preventDefault();
+        try {  
+            let data = await signInWithEmailAndPassword(auth, email, password)
+            console.log(data);
+            navigate("/");
+        } catch(error) {
+            console.log({error});
+            switch(error.code) {
+                case "auth/invalid-email": setError("유효하지 않은 이메일 주소입니다."); break;
+                case "auth/wrong-password": setError("비밀번호가 일치하지 않습니다."); break;
+                case "auth/user-not-found": setError("사용자를 찾을 수 없습니다."); break;
+                default : setError("Login Error"); break;
+            }
+        }
+    }
     return (
-        <div>
-            <h1>{"환영합니다"}</h1>
-            <p>로그인하기</p>
-            <GoogleSignInButton/>
-        </div>
+        <Container id="main-container" className="d-grid h-100 container">
+            <Form onSubmit={onSubmit} id="sign-in-form" className="text-center w-100">
+                <img
+                    className="mb-4 logo"
+                    src="img/temp-logo.png"
+                    alt="temp logo" />
+                    <h1 className="mb-3 fs-3 fw-normal">로그인</h1>
+                    <Form.Group controlId="sign-in-email-address">
+                        <Form.Control type="email" size="lg" placeholder="이메일 주소" autoComplete="username" className="position-relative" 
+                        onChange={onChange}/>
+                    </Form.Group>
+                    <Form.Group controlId="sign-in-password" className="mb-3">
+                        <Form.Control type="password" size="lg" placeholder="비밀번호" autoComplete="current-password" className="position-relative" 
+                        onChange={onChange}/>
+                    </Form.Group>
+                    <Form.Group className="d-flex justify-content-center mb-2">
+                        <Form.Check label="로그인 상태 유지" />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Text id="login-error-text">{error}</Form.Text>
+                    </Form.Group>
+                    <div id="button-div" className="d-grid">
+                        <Button type="submit" variant="primary" size="lg" className="w-100 mt-3 mb-3">로그인</Button>
+                        <GoogleSignInButton/>
+                    </div>
+            </Form> 
+        </Container>
     )
 
 }
