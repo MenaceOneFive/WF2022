9b7728a8af76c0560e51e543d502eb15306832ce
MenaceOneFive <menaceonefive@github.com>
2022-11-11 17:56:16 +0900

    항공권 검색 추가
    Signed-off-by: HoyeonLee <dlghdus122@gmail.com>

--- src/FirebaseWrapper/GoogleSignInButton.js
+++ src/FirebaseWrapper/GoogleSignInButton.js
@@ -1,5 +1,5 @@
 import {getAuth, GoogleAuthProvider, signInWithPopup} from "firebase/auth";
-import {Navigate, useNavigate} from "react-router-dom";
+import {Navigate } from "react-router-dom";
 import {useState} from "react";
 
 export const GoogleSignInButton = () => {
