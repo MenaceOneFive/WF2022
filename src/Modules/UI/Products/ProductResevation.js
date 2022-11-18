ce9e5d24f8fca9336ef0d60f4214a3252754a137
박첨벙 <114977536+plopPark@users.noreply.github.com>
2022-11-18 17:18:52 +0900

    예약을 위한 데이터 가져오기 확인

--- src/Modules/UI/Products/ProductResevation.js
+++ src/Modules/UI/Products/ProductResevation.js
@@ -25,30 +25,3 @@ export const ProductDetail = ({idx}) => {
         </>
     )
  }
-
- /*
- export const ProductReservation =() =>{
-    const params = useParams();
-    const [length, loading] = useRoomCollectionLength()
-    const roomID = params.productID
-    console.log(roomID)
-    if (loading)
-        return (<>{"ㄱㄷㄱㄷ"}</>)
-    if(length < roomID)
-        console.error("outOfIndexErr")
-    return(<ProductDetail idx={roomID}/>)
-}
-export const ProductDetail = ({idx}) => {
-    const [room, loading] = useRoom(idx)
-    if (loading)
-        return (<>{"Hold on"}</>)
-    return(
-        <>
-            <h1>숙소예약</h1>
-            <div>{room.id}</div>
-        </>
-    )
- }
-
- */
-
